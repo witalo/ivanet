e3a6daf24883a42f21005572dbb5d3590325be91
witalo <vent.wivf@gmail.com>
2023-10-09 18:36:06 -0500

    ksjsj

--- ivanet/settings.py
+++ ivanet/settings.py
@@ -76,12 +76,12 @@ WSGI_APPLICATION = 'ivanet.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
@@ -92,16 +92,16 @@ WSGI_APPLICATION = 'ivanet.wsgi.application'
 #         'PORT': 3306,
 #     }
 # }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'railway',
-        'USER': 'root',
-        'PASSWORD': '68wTTU3VK8jrZSgpLt4e',
-        'HOST': 'containers-us-west-159.railway.app',
-        'PORT': 7706,
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'railway',
+#         'USER': 'root',
+#         'PASSWORD': '68wTTU3VK8jrZSgpLt4e',
+#         'HOST': 'containers-us-west-159.railway.app',
+#         'PORT': 7706,
+#     }
+# }
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
